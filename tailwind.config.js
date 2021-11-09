2917a847799a43486d00f6516f3105c98f237744
Diego Albitres <diego.albitres@gmail.com>
2021-11-09 12:32:53 -0500

    Simplified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
+const defaultTheme =  require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./src/**/*.ts', './src/**/*.tsx'],
+  mode: 'aot', // 'aot' or 'jit'
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        inter:
-          '"Inter", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-        'inter-var':
-          '"Inter var", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        'inter-var': ['"Inter var"', ...defaultTheme.fontFamily.sans],
       },
     },
   },

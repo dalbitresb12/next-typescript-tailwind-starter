c35f01aa3692b31835717976953aedce58535d65
Diego Albitres <diego.albitres@gmail.com>
2021-11-09 12:33:07 -0500

    Moved Inter ref to _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render(): React.ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://rsms.me" />
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

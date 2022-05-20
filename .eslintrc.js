ddbb1a3974b29d45247e212857707fd07c5672c5
Diego Albitres <diego.albitres@gmail.com>
2022-05-20 03:20:16 -0500

    Initial setup of prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,95 +1,14 @@
-const rules = {
-  "indent": [
-    "error",
-    2,
-    { "SwitchCase": 1 }
-  ],
-  "linebreak-style": [
-    "error",
-    "unix"
-  ],
-  "semi": [
-    "error",
-    "always"
-  ],
-  "eol-last": [
-    "error",
-    "always"
-  ],
-  "eqeqeq": [
-    "error",
-    "always"
-  ],
-  "react/jsx-uses-react": "off",
-  "react/react-in-jsx-scope": "off",
-};
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
-  },
-  "extends": [
+  root: true,
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@next/next/core-web-vitals",
+    "plugin:react/jsx-runtime",
+    "next/core-web-vitals",
+    "prettier",
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  rules: {
+    eqeqeq: ["error", "always"],
   },
-  "plugins": [
-    "react"
-  ],
-  "settings": {
-    "react": {
-      "version": "detect",
-    },
-  },
-  "rules": {
-    ...rules,
-  },
-  "overrides": [
-    {
-      "files": ["src/**/*.ts", "src/**/*.tsx"],
-      "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-      },
-      "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@next/next/core-web-vitals",
-      ],
-      "parser": "@typescript-eslint/parser",
-      "parserOptions": {
-        "ecmaFeatures": {
-          "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-      },
-      "plugins": [
-        "react",
-        "@typescript-eslint"
-      ],
-      "settings": {
-        "react": {
-          "version": "detect",
-        },
-      },
-      "rules": {
-        ...rules,
-        "no-unused-vars": "off",
-        "react/prop-types": "off",
-      },
-    }
-  ],
 };

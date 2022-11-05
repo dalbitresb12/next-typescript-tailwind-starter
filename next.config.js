f4e635959a137c76039b248ede7980b8ead76692
Diego Albitres <diego.albitres@gmail.com>
2022-11-05 04:28:03 -0500

    Setup @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -6,5 +6,14 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: JSON.parse(process.env.STATIC_CONTENT_DOMAIN || "[]"),
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   productionBrowserSourceMaps: process.env.PRODUCTION_SOURCE_MAPS === "true",
 });

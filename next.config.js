9f66b110a975345a58bf4e6b6d5099e05fc0d007
Diego Albitres <diego.albitres@gmail.com>
2022-11-05 04:32:29 -0500

    Added IS_CLOUDFLARE env variable for image optimization

--- next.config.js
+++ next.config.js
@@ -2,10 +2,38 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+/**
+ * Parse the environment variable for the custom domains for Next.js Image Optimization
+ *
+ * @returns {string[]}
+ */
+const getImageDomains = () => {
+  try {
+    return JSON.parse(process.env.STATIC_CONTENT_DOMAIN);
+  } catch (err) {
+    return [];
+  }
+};
+
+/**
+ * Create the configuration for Next.js Image Optimization
+ *
+ * @returns {import("next/dist/shared/lib/image-config").ImageConfig}
+ */
+const createImageConfig = () => {
+  const isCloudflare = process.env.IS_CLOUDFLARE === "true";
+  // Cloudflare Pages doesn't support Image Optimization
+  if (isCloudflare) {
+    return { unoptimized: true };
+  }
+
+  return {
+    domains: getImageDomains(),
+  };
+};
+
 module.exports = withBundleAnalyzer({
-  images: {
-    domains: JSON.parse(process.env.STATIC_CONTENT_DOMAIN || "[]"),
-  },
+  images: createImageConfig(),
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/i,
